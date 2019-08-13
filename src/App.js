a7ad4cea4682b0daabf37dfea4451eb17cb1bfbd
jamie raven <jraventraining@gmail.com>
2019-08-13 12:45:18 -0700

    starting nav bar, gradient bg

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import React from 'react';
 import Container from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-// import Nav from 'react-bootstrap/Nav';
+import Nav from 'react-bootstrap/Nav';
 import Slider from './slider';
 import {Animated} from "react-animated-css";
-
+// import Navbar from "./components/Navbars/AdminNavbar"
+// import AdminNavbarLinks from "./components/Navbars"
 // import Carousel from './Carousel';
-
-
-
-// import logo from './logo.svg';
+// import MotionMenu from '@jessemc98/motion-menu';
+// import { SpacedMotionMenu } from '@jessemc98/motion-menu';
+import logo from './logo.svg';
 import './App.css';
+// import logo2 from './s1.png';
+// import logo3 from './s2.png';
+// import logo4 from './s3.png';
+// import logo5 from './s4.png';
 
 
+const btn = {iconSrc: logo}
+// const btn2 = {iconSrc: logo2}
+// const btn3 = {iconSrc: logo3}
+// const btn4 = {iconSrc: logo4}
 
 function App() {
   return (
@@ -22,11 +30,53 @@ function App() {
     //     <img src={logo} className="App-logo" alt="logo" />
     //     <p>
     //       Edit <code>src/App.js</code> and save to reload.  
-  <Container className="topper" style={{maxWidth:"100%"}}>
+  <Container className="topper" style={{maxWidth:"100%",
+    background: 'linear-gradient(to right bottom, 	#0080ff, #00ffff)'
+  }} >
+  {/* <h3>Corner menu example</h3>
+       
+            <MotionMenu
+              className="btmRight"
+              btns={[btn,btn,btn,btn]}
+              spaceBetween={22}
+              btnRadius={20}
+              mainBtnRadius={30}
+              angle={37}
+              angleOffset={45} /> */}
+      
 
+      <Nav 
+      // variant="pills"
+  activeKey="/home"
+  onSelect={selectedKey => alert(`selected ${selectedKey}`)}
+style={{
+  
+  background: 'linear-gradient(to right bottom, 	#0080ff, 	#0066cc'}}
+>
+  <Nav.Item>
+    <Nav.Link 
+    style={{color:"white"}}
+    href="/home">Home</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link style={{color:"white"}} href="mailto:hello@jamesravenscroft.com">Contact</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link style={{color:"white"}} href="https://swordmasters-august.firebaseapp.com">Recent Projects</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link style={{color:"white"}} eventKey="disabled" disabled>
+      James Ravenscroft III
+    </Nav.Link>
+  </Nav.Item>
+</Nav>
+    {/* <Navbar></Navbar> */}
+ <Animated animationIn="fadeInUpBig" animationOut="fadeOut" isVisible={true}>
+    <div>
 
   <Row>
-    <Col md={4}><Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
+    <Col md={4}>
+      <Animated animationIn="bounceInLeft" animationInDelay="550" animationOut="fadeOut" isVisible={true}>
     <div>
     <img src={require("./img/suit.PNG")} alt="suit"
     style={{width:"100%", float:"left", borderRadius:35}}/>
@@ -35,15 +85,15 @@ function App() {
 
     
     <Col  lg={{ span: 6}}>
-    <Animated animationIn="bounceInRight" animationOut="fadeOut" isVisible={true}>
+    <Animated animationIn="bounceInRight" animationInDelay="550" animationOut="fadeOut" isVisible={true}>
     <div>
-    <h1>Hello there!<br></br> My name is James Ravenscroft III<br>
+    <h1 style={{WebkitTextStrokeColor:"black", WebkitTextStrokeWidth:1}}>Hello there!<br></br> My name is James Ravenscroft III<br>
     </br>
     Full Stack Web Developer
     </h1>    </div></Animated>
 
     <p className="mainDescription"
-         style={{backgroundColor:"white", borderRadius:15, color:"black"}} 
+         style={{backgroundColor:"white", borderRadius:15, color:"black",width:"100%"}} 
           >
     Full Stack Developer- prior to web Development I've been a manger and business owner.<br></br>
      I've developed many client compliance systems and constantly pushed to improve <br></br>
@@ -53,10 +103,10 @@ function App() {
      logic functions.
      </p>
  </Col> 
-  </Row>
+  </Row></div></Animated>
   <br></br>
   <a href="https://calendly.com/jamesravenscroft3">
-    <h2>Schedule an appointment. Click here.</h2>
+    <h2 style={{color:"red", fontSize:60, WebkitTextStrokeColor:"black", WebkitTextStrokeWidth:2, WebkitFlexGrow:10}}>Schedule an appointment. Click here.</h2>
     
 
   </a>
@@ -64,13 +114,18 @@ function App() {
   <br></br>
   <Row>
     <Col><Animated animationIn="bounceInRight" animationOut="fadeOut" isVisible={true}>
-    <div><h1>A Boat Load of Know How</h1></div></Animated>
-    <h2>Front End Tech:</h2>
-    <p   style={{border:"blue", borderTopColor:"black" , borderWidth:10,borderBottomColor:"black", backgroundColor:"white", borderRadius:15, color:"black"}} >
+    <div><h1 style={{WebkitTextStrokeColor:"black", WebkitTextStrokeWidth:1}}>A Boat Load of Know How</h1></div></Animated>
+    <h2 style={{WebkitTextStrokeColor:"black", WebkitTextStrokeWidth:1}}>Front End Tech:</h2>
+    <p   style={{border:"blue", borderTopColor:"black" , borderWidth:10,borderBottomColor:"black", backgroundColor:"white", borderRadius:15, color:"black",
+          margin: 0,
+       padding:0,
+       width:"100%",
+       position:"relative"
+      }} >
       React, ESLint, BootStrap, Javascript, Jquery, CSS, Handlebars
     </p>
 
-    <h2>Back End Tech:</h2>
+    <h2 style={{WebkitTextStrokeColor:"black", WebkitTextStrokeWidth:1}}>Back End Tech:</h2>
     <p  style={{border:"blue", borderTopColor:"black" , borderWidth:10,borderBottomColor:"black", backgroundColor:"white", borderRadius:15, color:"black"}} >NodeJS, MySQL, FireBase, Auth0, AJAX, Mongoose, Sequelize, Express
     </p>
      
@@ -79,7 +134,7 @@ function App() {
 
   </Row>
   <Row>    
-    <Col><h1>Contact:</h1><br></br><p  style={{border:"blue", borderTopColor:"black" , borderWidth:10,borderBottomColor:"black", backgroundColor:"white", borderRadius:15}} >
+    <Col><h1 style={{WebkitTextStrokeColor:"black", WebkitTextStrokeWidth:1}}>Contact:</h1><br></br><p  style={{border:"blue", borderTopColor:"black" , borderWidth:10,borderBottomColor:"black", backgroundColor:"white", borderRadius:15}} >
     <p style={{color:"black", fontSize:"150%"}}>email: hello@jamesravenscroft3.com
     <br></br>
     phone: 781-727-3848</p>
@@ -92,9 +147,17 @@ function App() {
 </Row>
 <br></br>
 <Row>
-  <h1>PROJECTS:</h1><br></br>
-  <h1>
-Oasis RPG: SwordMasters of Ginaz</h1>
+  <h1 style={{WebkitAlignContent:"center", WebkitTextStrokeColor:"black", WebkitTextStrokeWidth:1}}>PROJECTS:</h1><br></br>
+  </Row><Row>
+
+  <h1 style={{WebkitTextStrokeColor:"black", WebkitTextStrokeWidth:1}}>
+Oasis RPG: SwordMasters of Ginaz</h1></Row><Row>
+<br></br>
+<h1 style={{WebkitTextStrokeColor:"black", WebkitTextStrokeWidth:1}}>
+
+Web Based Desktop RPG
+</h1>
+
 
 <a href="https://morning-refuge-74795.herokuapp.com/"><Animated animationIn="fadeIn" 
 // animationInDelay={10000} 
@@ -106,7 +169,9 @@ animationOut="fadeInDownBig" isVisible={true}>
 <br></br><h2 style={{float: "left", marginLeft:"25%"}}>Made with React</h2>
 <img src={require("./img/React.js_logo-512.png")} alt="reactLogo" style={{height:50, marginRight:"20%"}}/>
 <br></br>
-<iframe src="https://player.vimeo.com/video/346965384" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+<iframe src="https://player.vimeo.com/video/346965384" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen
+style={{marginLeft:"25%"}}
+></iframe>
 
 </Row>
 <Row>
@@ -118,7 +183,7 @@ animationOut="fadeInDownBig" isVisible={true}>
 <Row><h1>
   ReasonToDrink.Fun</h1><a href="https://ReasonToDrink.Fun">
   <img src={require("../src/img/cheers.gif")} alt="reasonToDrinkLogo" width="40%" 
-  style={{  borderRadius: 85
+  style={{  borderRadius: 85, width:"100%"
  }}
    />
 </a>
