11ed91d254b74b383ddc9054e173bf2a3e7241a8
James Ravenscroft <hello@ravenscroftgames.net>
2019-11-29 17:16:29 -0800

    fixed header issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ function Header (){
 
     return(
     <div>
-        <Container className="bg" style={{ whiteSpace: 'normal'}}>
+        <Container className="bg" style={{whiteSpace: 'normal'}}>
 {/* <Animated animationIn="fadeInUpBig" animationOut="fadeOut" isVisible={true}>
     <Col> */}
 
@@ -18,32 +18,35 @@ function Header (){
             <Animated animationIn="bounceInLeft" animationInDelay="550" animationOut="fadeOut" isVisible={true}>
           <div>
           <img src={require("../img/suit.PNG")} alt="suit"
-          style={{width:"40%", float:"left", borderRadius:35, maxWidth:"60%"}}/>
+          style={{width:"30%", float:"left", borderRadius:35, maxWidth:"40%"}}/>
           </div></Animated>
           </Col>
 
           
-          <Col  lg={{ span: 6,  whiteSpace: 'normal', float:"right"}}>
+         
           <Animated animationIn="bounceInRight" animationInDelay="550" animationOut="fadeOut" isVisible={true}>
-          
-          <Col  style={{whiteSpace:"normal"}}>
-          <h1 style={{WebkitTextStrokeColor:"black", maxWidth:"55%", float:"right", marginTop:"-40%", WebkitTextStrokeWidth:1,  whiteSpace: 'normal'}}>
+          <Col  lg={{ span: 6,  whiteSpace: 'normal', float:"right"}}>
+          {/* <Col  style={{whiteSpace:"normal"}}> */}
+          <h1 style={{WebkitTextStrokeColor:"black", maxWidth:"55%", float:"right", marginTop:"-30%", WebkitTextStrokeWidth:1,  whiteSpace: 'normal'}}>
               Hello there!<br></br> My name is James Ravenscroft III<br>
           </br>
           Full Stack Web Developer
-          </h1>    </Col></Animated></Col>
-      <Col>
-        <p className="text-justify"
-            style={{ whiteSpace: 'normal', color:"white", fontSize:"150%", marginLeft:"3%", WebkitTextStrokeColor:"black", WebkitTextStrokeWidth:.5}} 
+          </h1>    
+          {/* </Row>
+          <Row> */}
+            {/* <Col md={8}> */}
+        <p className="text-justify" style={{ whiteSpace: 'normal', color:"white", fontSize:"150%", WebkitTextStrokeColor:"black", WebkitTextStrokeWidth:.5}} 
               >
-              I am currently working as Coding Instructor at The Coder School of La Jolla, California. 
+        I am currently working as Coding Instructor at The Coder School of La Jolla, California. 
         </p>
-        <p style={{ whiteSpace: 'normal', color:"white", fontSize:"150%", WebkitTextStrokeColor:"black", WebkitTextStrokeWidth:.5}}>
+        <p style={{whiteSpace: 'normal', color:"white", fontSize:"150%", WebkitTextStrokeColor:"black", WebkitTextStrokeWidth:.5}}>
             Some examples of my work are listed below, which are a mix of Node.JS and React.</p><br/>
             <p style={{ whiteSpace: 'normal', color:"white", fontSize:"150%", WebkitTextStrokeColor:"black", WebkitTextStrokeWidth:.5}}>
             When I'm not coding I enjoy writing for independent films, bike riding and spending time with my family.
-            </p>   
-            </Col>
+            </p>     </Col>
+            {/* </Col> */}
+            </Animated>
+         
   </Row>
   {/* </Col></Animated> */}
 </Container>
