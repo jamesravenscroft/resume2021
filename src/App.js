0c74528eb7da28798d69b8f8c809ed963538f214
James Ravenscroft <hello@ravenscroftgames.net>
2019-12-29 09:12:18 -0800

    added js to components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Header from './components/header';
-import About from './components/about';
-import Resume from './components/resume';
-import Portfolio from './components/portfolio';
-import Testimonials from  './components/testimonials';
+import Header from './components/header.js';
+import About from './components/about.js';
+import Resume from './components/resume.js';
+import Portfolio from './components/portfolio.js';
+import Testimonials from  './components/testimonials.js';
 // import ContactUs from './components/contactus';
-import Footer from './components/footer';
+import Footer from './components/footer.js';
 function App() {
   return (
     <div className="App">
